96ea80862692ee92aed7a9b72eb6f3d33af28f2a
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-11-21 14:59:37 +0100

    add: esercizio email list

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      numberOfEmail: 0,
+      emails: [],
+    };
+  },
+  created() {
+  },
+  methods: {
+    generateEmails() {
+      while(this.numberOfEmail < 10){
+        axios
+        .get(
+          `https://flynn.boolean.careers/exercises/api/random/mail`
+        )
+        .then((resp) => {
+          this.emails.push(resp.data.response);
+        });
+        this.numberOfEmail++
+      }
+      this.emails = [];
+      this.numberOfEmail = 0
+    },
+  }
+}).mount("#app");
